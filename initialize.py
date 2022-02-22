da9ca4597d4d976cbb620364aa53558eced3e7a4
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2022-02-22 12:42:24 +0100

    initialize downloads model file

--- initialize.py
+++ initialize.py
@@ -1,2 +1,7 @@
 # TODO: download yolo model if not available
 # https://github.com/OlafenwaMoses/ImageAI/releases/tag/1.0/
+
+import requests
+url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5'
+r = requests.get(url, allow_redirects=True)
+open('./models/yolo.h5', 'wb').write(r.content)
