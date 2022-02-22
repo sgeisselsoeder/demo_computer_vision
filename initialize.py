3d71d13b3b40e6fcb2a18d1c91d551143f9cc5e7
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2022-02-22 12:48:34 +0100

    only download model file if missing and print some first time user hints

--- initialize.py
+++ initialize.py
@@ -1,7 +1,15 @@
-# TODO: download yolo model if not available
-# https://github.com/OlafenwaMoses/ImageAI/releases/tag/1.0/
-
 import requests
-url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5'
-r = requests.get(url, allow_redirects=True)
-open('./models/yolo.h5', 'wb').write(r.content)
+import os.path
+
+print("Don't forget to:")
+print("python3 -m venv .venvthis ")
+print("source .venvthis/bin/activate")
+print("pip install -r requirements.txt")
+
+yolo_model_file = './models/yolo.h5'
+if not os.path.isfile(yolo_model_file):
+    url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5'
+    r = requests.get(url, allow_redirects=True)
+    open(yolo_model_file, 'wb').write(r.content)
+else:
+    print("Yolo model file already found at " + str(yolo_model_file))
