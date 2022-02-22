993620b407ee92380d6ddba0428224f8ab2adbe7
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2022-02-22 12:49:25 +0100

    adds hint for the impatient

--- initialize.py
+++ initialize.py
@@ -8,6 +8,7 @@ print("pip install -r requirements.txt")
 
 yolo_model_file = './models/yolo.h5'
 if not os.path.isfile(yolo_model_file):
+    print("Downloading model file. This may take a while.")
     url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5'
     r = requests.get(url, allow_redirects=True)
     open(yolo_model_file, 'wb').write(r.content)
