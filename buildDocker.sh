907fbf5b9db8312e4cb066bf1fe2951f55c19063
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2022-02-24 08:48:45 +0100

    solved the x and display forward issue

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker build -t demo_computer_vision .
 
-# sudo docker run --runtime nvidia -it --rm --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 --network host --device /dev/video0 -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix demo_computer_vision
+# sudo docker run --runtime nvidia -it --rm --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 --network host --device /dev/video0 -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix:/tmp/.X11-unix demo_computer_vision
+
