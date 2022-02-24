9d833144f25b5db7e615dad904e5ebdd86049432
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2022-02-24 09:09:45 +0100

    moves some instructions to readme file

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 docker build -t demo_computer_vision .
 
-# sudo docker run --runtime nvidia -it --rm --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 --network host --device /dev/video0 -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix:/tmp/.X11-unix demo_computer_vision
+# relevant info from
+# https://www.cloudsavvyit.com/10520/how-to-run-gui-applications-in-a-docker-container/
+# and
+# https://catalog.ngc.nvidia.com/orgs/nvidia/containers/tensorflow
 
+# sudo docker run --runtime nvidia -it --rm --gpus all --ipc=host --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 --network host --device /dev/video0 -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix:/tmp/.X11-unix demo_computer_vision
+
+# don't forget to supervise gpu load
+# watch -n 2 nvidia-smi
